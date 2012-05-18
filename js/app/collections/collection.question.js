ec91748ab6a120db51faecc657b7ee66ca6e42af
James Burns <jameburn@gmail.com>
2012-05-18 13:22:46 -0400

    added logo

--- js/app/collections/collection.question.js
+++ js/app/collections/collection.question.js
@@ -2,13 +2,10 @@
 
 	Questions.Collection = Backbone.Collection.extend({
 
-<<<<<<< HEAD
 	
 
 		initialize : function(options)
-=======
-			initialize : function(options)
->>>>>>> html
+
 		{
 			_.extend(this,options);
 			console.log('questions collection init')

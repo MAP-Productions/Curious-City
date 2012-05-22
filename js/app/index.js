1b81a347fdfc7c5ad35965c110fcaede27edcef2
Joseph Bergen <jbergen@gmail.com>
2012-05-22 19:23:02 -0400

    how it works now animates open/closed. fixes #6

--- js/app/index.js
+++ js/app/index.js
@@ -7,7 +7,11 @@ jQuery(function($)
 	Curiouscity.init();
 
 	$('#how-it-works-expander').click(function(){
-		$('#how-it-works').toggleClass('hide')
+		$('#how-it-works-expander i').toggleClass('down');
+		$('#how-it-works').toggleClass('open');
+		
+		if( $('#how-it-works').hasClass('open') ) $('#how-it-works').show('blind',{direction:'vertical'},500);
+		else $('#how-it-works').hide('blind',{direction:'vertical'},500);
 	})
 	
 	$('#nav-about').click(function(){

edf6ed5b2f52063a9eeffb4f8e1e7f54f7ea7199
Joseph Bergen <jbergen@gmail.com>
2012-05-24 14:59:17 -0400

    validation on ask form added

--- js/app/models/model.question.ask.js
+++ js/app/models/model.question.ask.js
@@ -15,6 +15,19 @@
 		initialize : function()
 		{
 		},
+		
+		validate : function(attrs)
+		{
+			var errors = '';
+			var email_check = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
+
+			if(attrs.question=='') errors += '.question-text,';
+			if(attrs.name =='') errors+= '.name-text,';
+			if(attrs.email_confirm != attrs.email || attrs.email == '' || !email_check.test(attrs.email) )
+				errors+= '.email-confirm,.email-main';
+			
+			return errors;
+		}
 
 
 	});

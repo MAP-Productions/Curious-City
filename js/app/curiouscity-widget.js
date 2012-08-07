1b4544c509054dc9d17fe4d2d27ccd459676500f
James Burns <jameburn@gmail.com>
2012-08-07 10:04:54 -0400

    updates

--- js/app/curiouscity-widget.js
+++ js/app/curiouscity-widget.js
@@ -48,7 +48,7 @@ app: _.extend({
 					}
 				});
 				
-			if(_.isUndefined(this.questionsCollection.get(voteData.yourvote))) {
+			if(!_.isUndefined(this.questionsCollection.get(voteData.yourvote))) {
 			
 			console.log(this.questionsCollection.get(voteData.yourvote));
 			this.questionsCollection.canvote=true;

b64180b10e51e42b53204e12962a204ce9cac186
James Burns <jameburn@gmail.com>
2012-09-17 13:47:04 -0400

    archive updates

--- index.php
+++ index.php
@@ -601,9 +601,9 @@
 	</div><!-- .container -->
 	
 	<!-- Application source DEV-->
-		  <script data-main="js/loaders/index.js" src="js/lib/require.js"></script> 
+	<!--	  <script data-main="js/loaders/index.js" src="js/lib/require.js"></script> -->
 	<!-- Production -->
-<!-- <script data-main="js_min/index.js" src="js/lib/require.js"></script> -->
+ 	<script data-main="js_min/index.js" src="js/lib/require.js"></script> -->
 	
 </body>
 </html>
